4c35f24df3543666e6e95f77f35ab1c43cda2491
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-04-29 11:24:35 +0530

    1st node server app.
    what i learn
    
    1. require in var
    2. http
    3. createServer
    4. function(req,res)
    5. res.writeHead
    6. res.end
    7. .listen(8080)
    8. http: (not https:)

--- 1_day_29April/node.js
+++ 1_day_29April/node.js
@@ -1,3 +1,5 @@
+/* Practice:
+---------------------------------------------------------------
 var http = require('http');
 
 http
@@ -7,4 +9,24 @@ http
   })
   .listen(8080); //Y: listen client request on port 8080.
 
-//DX: make sure to use http:// and not https://
+DX: make sure to use http:// and not https://
+---------------------------------------------------------------
+*/
+var http = require('http'); //Y: 1
+
+http
+  .createServer(function (req, res) {
+    //Y: 2
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello There Again !');
+  })
+  .listen(8080);
+
+/* Y:
+ * 1. we require http
+ * 2. we create server Application with http consisting of 1 function.
+ * 3. when 'req' is recieved function is returingng 'res'.
+ * 4. .listen(8080); will host on http://localhost:8080
+ *
+ *
+ * */
