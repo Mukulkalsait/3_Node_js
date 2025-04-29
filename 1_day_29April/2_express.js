66bbd0076ad05510a2632896bb1c20e5d305d49e
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-04-29 11:59:26 +0530

    2nd Express basics

---
+++ 1_day_29April/2_express.js
@@ -0,0 +1,17 @@
+var express = require('express'); //     Y: 1. require calling.
+var app = express(); //            Y: 2. OBJECT creation.
+
+/* NOTE:  now once 1 and 2 are Done.
+ *  module can be INVOKED.
+ *-------------------------------
+ *     USE OF EXPRESS:
+ *-------------------------------
+ * TEMPLATING ENGINE is an approach for injecting values in an application
+ *  by picking up data from data files.
+ * This concept is pretty famous in Angular JS
+ * wherein the curly braces
+ *        {{ key }}
+ * is used to substitutes values in the web page.
+ * The word 'key' in the curly braces basically denotes
+ * the variable which will be substituted by a value when the page is displayed.
+ *  */
